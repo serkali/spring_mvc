d2026622896a3d9ca4acd55d3a81ff1d2f2574b8
serkali88 <serkali88@gmail.com>
2021-07-02 21:55:29 +0400

    Java pre-project. Задача 2.2.2

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class CarService {
-    public static List<Car> carsCount(List<Car> list, int number) {
-        if (number == 0 || number > 5) return list;
-        return list.stream().limit(number).collect(Collectors.toList());
+    public static List<Car> carsCount(List<Car> list, int count) {
+        if (count == 0 || count > 5) return list;
+        return list.stream().limit(count).collect(Collectors.toList());
     }
 }
 
